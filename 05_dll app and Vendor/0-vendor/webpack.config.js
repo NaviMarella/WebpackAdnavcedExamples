0419115aa325d5dc7e800e4c41ecc1b534699896
Navi Marella <navi.marella@gmail.com>
2018-09-28 09:42:18 -0600

    Added a sample plugin to test plugin Api

--- 05_dll app and Vendor/0-vendor/webpack.config.js
+++ 05_dll app and Vendor/0-vendor/webpack.config.js
@@ -2,6 +2,24 @@ var path = require("path");
 var webpack = require("webpack");
 var cleanWebpackPlugin = require('clean-webpack-plugin');
 
+// A JavaScript class.
+class MyExampleWebpackPlugin {
+	// Define `apply` as it's prototype method which is supplied with compiler as it's argument
+	apply(compiler) {
+		// Specify the event hook to attach to
+		compiler.hooks.compile.tap(
+			'MyExampleWebpackPlugin',
+			(compilation, callback) => {
+				console.log('This is an example plugin!');
+				console.log('Here’s the `compilation` object which represents a single build of assets:', compilation);
+				console.log(compilation, callback);
+
+				callback();
+			}
+		);
+	}
+}
+
 module.exports = {
 	mode: "none", //development || "production",
 	context: __dirname,
@@ -16,6 +34,7 @@ module.exports = {
 		new webpack.DllPlugin({
 			name: "vendor_lib_[hash]",
 			path: path.resolve(__dirname, "dist/vendor-manifest.json")
-		})
+		}),
+		new MyExampleWebpackPlugin()
 	]
 };
--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,9 @@
 //var currentModule = require('./02_chunkhash/webpack.config');
 //var currentModule = require('./03_codesplitting css bundle/webpack.config');
 //var currentModule = require('./04_common chunk and vendor chunk/webpack.config');
-//var currentModule = require('./05_dll app and Vendor/1-app/webpack.config');
+var currentModule = require('./05_dll app and Vendor/0-vendor/webpack.config');
 //var currentModule = require('./06_Aggresive splitting/webpack.config');
 //var currentModule = require('./07_spliting chunks/webpack.config');
 
-var currentModule = require('./08_scope hoisting/webpack.config');
+//var currentModule = require('./08_scope hoisting/webpack.config');
 module.exports = currentModule;
