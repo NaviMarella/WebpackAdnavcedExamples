e8618f8dc0aaeeae3fb694b50e9613d97c7670c5
Navi Marella <navi.marella@gmail.com>
2018-10-01 11:56:09 -0600

    added debug command using node-nightly

--- 05_dll app and Vendor/0-vendor/webpack.config.js
+++ 05_dll app and Vendor/0-vendor/webpack.config.js
@@ -1,40 +1,52 @@
-var path = require("path");
-var webpack = require("webpack");
+var path = require('path');
+var webpack = require('webpack');
 var cleanWebpackPlugin = require('clean-webpack-plugin');
 
-// A JavaScript class.
-class MyExampleWebpackPlugin {
-	// Define `apply` as it's prototype method which is supplied with compiler as it's argument
+class FileListPlugin {
 	apply(compiler) {
-		// Specify the event hook to attach to
-		compiler.hooks.compile.tap(
-			'MyExampleWebpackPlugin',
-			(compilation, callback) => {
-				console.log('This is an example plugin!');
-				console.log('Here’s the `compilation` object which represents a single build of assets:', compilation);
-				console.log(compilation, callback);
+		// emit is asynchronous hook, tapping into it using tapAsync, you can use tapPromise/tap(synchronous) as well
+		compiler.hooks.emit.tapAsync('FileListPlugin', (compilation, callback) => {
+			// Create a header string for the generated file:
+			var filelist = 'In this build:\n\n';
 
-				callback();
+			// Loop through all compiled assets,
+			// adding a new line item for each filename.
+			for (var filename in compilation.assets) {
+				filelist += '- ' + filename + '\n';
 			}
-		);
+
+			// Insert this list into the webpack build as a new file asset:
+			compilation.assets['filelist.md'] = {
+				source: function() {
+					return filelist;
+				},
+				size: function() {
+					return filelist.length;
+				}
+			};
+
+			console.log(compilation.assets);
+
+			callback();
+		});
 	}
 }
 
 module.exports = {
-	mode: "none", //development || "production",
+	mode: 'none', //development || "production",
 	context: __dirname,
-	entry: ["../node_modules/example-vendor"],
+	entry: ['../node_modules/example-vendor'],
 	output: {
-		filename: "vendor.js", // best use [hash] here too
-		path: path.resolve(__dirname, "dist"),
-		library: "vendor_lib_[hash]"
+		filename: 'vendor.js', // best use [hash] here too
+		path: path.resolve(__dirname, 'dist'),
+		library: 'vendor_lib_[hash]'
 	},
 	plugins: [
 		new cleanWebpackPlugin(path.join(__dirname, 'dist')),
 		new webpack.DllPlugin({
-			name: "vendor_lib_[hash]",
-			path: path.resolve(__dirname, "dist/vendor-manifest.json")
+			name: 'vendor_lib_[hash]',
+			path: path.resolve(__dirname, 'dist/vendor-manifest.json')
 		}),
-		new MyExampleWebpackPlugin()
+		new FileListPlugin()
 	]
 };
